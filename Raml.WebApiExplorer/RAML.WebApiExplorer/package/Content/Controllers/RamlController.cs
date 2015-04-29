cf581e37558f340616a87f39bc3e6226ceb8c09d
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-04-29 14:56:55 -0300

    download the raml option

--- Raml.WebApiExplorer/RAML.WebApiExplorer/package/Content/Controllers/RamlController.cs
+++ Raml.WebApiExplorer/RAML.WebApiExplorer/package/Content/Controllers/RamlController.cs
@@ -14,16 +14,27 @@ namespace RAML.WebApiExplorer
 
 		public ActionResult Raml()
 		{
-			var config = GlobalConfiguration.Configuration;
-			var apiExplorer = config.Services.GetApiExplorer();
-			var apiExplorerService = new ApiExplorerService(apiExplorer, config.VirtualPathRoot);
-			var ramlDocument = apiExplorerService.GetRaml();
-			var ramlContents = new RamlSerializer().Serialize(ramlDocument);
-
+            var ramlContents = GetRamlContents();
 			return File(Encoding.UTF8.GetBytes(ramlContents), "text/raml");
 		}
 
-		public ActionResult OAuth1()
+        public ActionResult Download()
+        {
+            var ramlContents = GetRamlContents();
+            return File(Encoding.UTF8.GetBytes(ramlContents), "text/raml", "generated.raml");
+        }
+
+	    private static string GetRamlContents()
+	    {
+	        var config = GlobalConfiguration.Configuration;
+	        var apiExplorer = config.Services.GetApiExplorer();
+	        var apiExplorerService = new ApiExplorerService(apiExplorer, config.VirtualPathRoot);
+	        var ramlDocument = apiExplorerService.GetRaml();
+	        var ramlContents = new RamlSerializer().Serialize(ramlDocument);
+	        return ramlContents;
+	    }
+
+	    public ActionResult OAuth1()
 		{
 			return View();
 		}
