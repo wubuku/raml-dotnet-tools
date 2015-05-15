6b3406535a5df9d75c512ab21511993dd457bbf7
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-05-15 12:29:39 -0300

    remove media type extension param from names remove duplicated NetNamingMapper class

--- Raml.Tools/MethodsGeneratorBase.cs
+++ Raml.Tools/MethodsGeneratorBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using Raml.Common;
 using Raml.Parser.Expressions;
 using Raml.Tools.Pluralization;
 
--- Raml.Tools/ObjectParser.cs
+++ Raml.Tools/ObjectParser.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Raml.Common;
 
 namespace Raml.Tools
 {
