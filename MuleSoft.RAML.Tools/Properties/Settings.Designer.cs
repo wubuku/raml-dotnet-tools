e232f8fa30b27871b072b38c3cdf7ebcb372f295
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-06-09 13:20:05 -0300

    Fix issue #5 in github (.NET client is sending bool values capitalized - should be lowercased)

--- MuleSoft.RAML.Tools/Properties/Settings.Designer.cs
+++ MuleSoft.RAML.Tools/Properties/Settings.Designer.cs
@@ -124,7 +124,7 @@ namespace MuleSoft.RAML.Tools.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.1")]
         public string ClientTemplatesVersion {
             get {
                 return ((string)(this["ClientTemplatesVersion"]));
