051e29ec82cd6b972131790d522762079d63a3b6
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-07-23 15:02:07 -0300

    Changes to Custom Properties Editor UI

--- Raml.Common/RamlPropertiesEditor.xaml.cs
+++ Raml.Common/RamlPropertiesEditor.xaml.cs
@@ -1,11 +1,38 @@
-﻿namespace Raml.Common
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Raml.Common.Annotations;
+
+namespace Raml.Common
 {
     /// <summary>
     /// Interaction logic for RamlPropertiesEditor.xaml
     /// </summary>
-    public partial class RamlPropertiesEditor
+    public partial class RamlPropertiesEditor : INotifyPropertyChanged
     {
         private string ramlPath;
+        private string ns;
+        private string source;
+
+        public string Namespace
+        {
+            get { return ns; }
+            set
+            {
+                ns = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Source
+        {
+            get { return source; }
+            set
+            {
+                source = value; 
+                OnPropertyChanged();
+            }
+        }
+
         public RamlPropertiesEditor()
         {
             InitializeComponent();
@@ -14,18 +41,35 @@
         public void Load(string ramlPath)
         {
             this.ramlPath = ramlPath;
-            PropertyGrid.SelectedObject = RamlPropertiesManager.Load(ramlPath);
+            var ramlProperties = RamlPropertiesManager.Load(ramlPath);
+            Namespace = ramlProperties.Namespace;
+            Source = ramlProperties.Source;
         }
 
         private void SaveButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            RamlPropertiesManager.Save((RamlProperties)PropertyGrid.SelectedObject, ramlPath);
+            var ramlProperties = new RamlProperties
+            {
+                Namespace = Namespace,
+                Source = Source
+            };
+            
+            RamlPropertiesManager.Save(ramlProperties, ramlPath);
             Close();
         }
 
         private void CancelButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             Close();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
