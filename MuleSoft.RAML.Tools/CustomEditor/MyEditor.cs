b10dd88a4a49ed092b25fd4b1fae0508704efbd5
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-08-25 16:52:15 -0300

    test integration with Ace Editor

--- MuleSoft.RAML.Tools/CustomEditor/MyEditor.cs
+++ MuleSoft.RAML.Tools/CustomEditor/MyEditor.cs
@@ -45,7 +45,7 @@ namespace MuleSoft.RAML.Tools.CustomEditor
             // TODO: detect drive ! also, any rights will work ?
             var fileName = "file://127.0.0.1/c$" + Path.GetFileName(filePath);
             //var fileName = Path.GetFileName(filePath);
-            webBrowser1.Document.InvokeScript("load", new object[] { Path.GetDirectoryName(filePath), fileName });
+            //webBrowser1.Document.InvokeScript("load", new object[] { Path.GetDirectoryName(filePath), fileName });
         }
 
         private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
