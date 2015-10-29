9cd05c5e418a10707ebe6d565d1f48b3435ec019
Pedro Wood <mail_pedro@yahoo.com.ar>
2015-10-29 14:44:00 -0300

    use WebApi Core version 5.2.0 for portability

--- MuleSoft.RAML.Tools/Properties/Settings.Designer.cs
+++ MuleSoft.RAML.Tools/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MuleSoft.RAML.Tools.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -61,7 +61,7 @@ namespace MuleSoft.RAML.Tools.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("5.1.2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("5.2.0")]
         public string WebApiCorePackageVersion {
             get {
                 return ((string)(this["WebApiCorePackageVersion"]));
