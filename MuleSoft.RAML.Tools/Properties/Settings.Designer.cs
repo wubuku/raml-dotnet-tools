706680e6510a0a7b44df9f17875f02926a375baf
Pedro Wood <mail_pedro@yahoo.com.ar>
2016-01-13 14:17:20 -0300

    update RAML.API.Core version

--- MuleSoft.RAML.Tools/Properties/Settings.Designer.cs
+++ MuleSoft.RAML.Tools/Properties/Settings.Designer.cs
@@ -88,7 +88,7 @@ namespace MuleSoft.RAML.Tools.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.9.6")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.9.7")]
         public string RAMLApiCorePackageVersion {
             get {
                 return ((string)(this["RAMLApiCorePackageVersion"]));
