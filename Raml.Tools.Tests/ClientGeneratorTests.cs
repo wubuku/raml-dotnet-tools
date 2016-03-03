211c8f0f55605699ab747515ed1b61429fd814e6
Pedro Wood <mail_pedro@yahoo.com.ar>
2016-03-03 10:48:39 -0300

    tree/recursive json schema JSON.NET issue

--- Raml.Tools.Tests/ClientGeneratorTests.cs
+++ Raml.Tools.Tests/ClientGeneratorTests.cs
@@ -515,6 +515,16 @@ namespace Raml.Tools.Tests
             Assert.AreEqual(2, model.Objects.Count(o => o.Name.Contains("Patch")));
         }
 
+        [Test, Ignore] // JSON.NET issue
+        public async Task ShouldHandleJsonSchemaRecursiveTree()
+        {
+            var raml = await new RamlParser().LoadAsync("files/tree-issue63.raml");
+            var model = new ClientGeneratorService(raml, "test", "TargetNamespace").BuildModel();
+
+            Assert.IsNotNull(model);
+            Assert.AreEqual(1, model.Objects.Count());
+        }
+
         [Test]
         public async Task ShouldGenerateRootResource()
         {
@@ -522,7 +532,6 @@ namespace Raml.Tools.Tests
             Assert.IsTrue(model.Classes.Any(c => c.Name == "RootUrl"));
             Assert.AreEqual(3, model.Classes.First(c => c.Name == "RootUrl").Methods.Count);
         }
-
         private static string GetXml(string comment)
         {
             if (string.IsNullOrWhiteSpace(comment))
