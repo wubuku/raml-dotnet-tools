a23014850df65e506c68ce30eba7e973a853cea0
Pedro Wood <mail_pedro@yahoo.com.ar>
2016-03-29 13:54:51 -0300

    assembly metada and nuspec

--- MuleSoft.RAMLGen/Properties/AssemblyInfo.cs
+++ MuleSoft.RAMLGen/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MuleSoft.RAML.Tools.CLI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("RAMLGen")]
+[assembly: AssemblyDescription("RAMLGenerator is a command line for generating client and server code based on a RAML spec.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MuleSoft.RAML.Tools.CLI")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("MuleSoft Inc")]
+[assembly: AssemblyProduct("RAMLGen")]
+[assembly: AssemblyCopyright("MuleSoft Inc Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
